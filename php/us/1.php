ff696193e5759b1dac63a8d8c3d197ba5649c715
NilsonIslen <nilislen@gmail.com>
2023-06-08 23:36:29 +0000

    arc us delet

--- php/us/1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$i=1; 
-$e='nilislen@gmail.com'; 
-$ee='2464c14cd144996cdd25d6124e66fcc9'; 
-$s=981; 
-$k='39999869675553cefe387ad81e6e0195'; 
-$m='06'; 
-?> 
--- php/us/1/index.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$i=1; 
-$e='nilislen@gmail.com'; 
-$ee='2464c14cd144996cdd25d6124e66fcc9'; 
-$k='39999869675553cefe387ad81e6e0195'; 
-?> 
